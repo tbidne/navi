tbidne/navi

commit 991ad9a30be3fb5c8d86c13a0c1e3b01fc8b813b

Update dependencies
Also improve warning/error handling.

app/Main.hs
@@ -10,7 +10,7 @@ import Effects.FileSystem.HandleWriter (MonadHandleWriter (withBinaryFile), die)
 import Effects.FileSystem.PathReader qualified as Dir
 import Effects.FileSystem.PathWriter (MonadPathWriter)
 import Effects.FileSystem.PathWriter qualified as Dir
-import Effects.FileSystem.Utils (encodeFpToOsThrowM, osp, (<</>>!))
+import Effects.FileSystem.Utils (encodeFpToOsThrowM, encodeFpToValidOsThrowM, osp)
 import Effects.Time (MonadTime)
 import Effects.Time qualified as Time
 import GHC.Conc.Sync (setUncaughtExceptionHandler)
@@ -110,8 +110,11 @@ withLogHandle logging onMHandle = do
       -- handle large log dir
       handleLogSize xdgState sizeMode
 
-      currTime <- fmap replaceSpc <$> Time.getSystemTimeString
-      logFile <- xdgState <</>>! (currTime <> ".log")
+      currTimeOs <-
+        encodeFpToValidOsThrowM
+          . fmap replaceSpc
+          =<< Time.getSystemTimeString
+      let logFile = currTimeOs <> [osp|.log|]
       stateExists <- Dir.doesDirectoryExist xdgState
       unless stateExists (Dir.createDirectoryIfMissing True xdgState)
       renameIfExists logFile

.github/workflows/ci.yaml
@@ -17,8 +17,8 @@ jobs:
       fail-fast: false
       matrix:
         ghc:
-          - "9.4.6"
-          - "9.6.2"
+          - "9.4.7"
+          - "9.6.3"
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v3
@@ -26,6 +26,9 @@ jobs:
       - uses: haskell/actions/setup@v2
         with:
           ghc-version: ${{ matrix.ghc }}
+
+      - name: Configure
+        run: cabal configure --enable-tests --ghc-options -Werror
 
       - name: Compile
         run: cabal build

cabal.project
@@ -2,10 +2,21 @@ packages: .
 
 package navi
   ghc-options:
-    -Wall -Wcompat -Werror -Widentities -Wunused-binds
-    -Wincomplete-record-updates -Wincomplete-uni-patterns
-    -Wmissing-home-modules -Wmissing-export-lists -Wpartial-fields
-    -Wredundant-constraints -Wunused-packages -Wmissing-deriving-strategies
+    -Wall -Wcompat
+    -Widentities
+    -Wincomplete-record-updates
+    -Wincomplete-uni-patterns
+    -Wmissing-deriving-strategies
+    -Wmissing-export-lists
+    -Wmissing-exported-signatures
+    -Wmissing-home-modules
+    -Wpartial-fields
+    -Wprepositive-qualified-module
+    -Wredundant-constraints
+    -Wunused-binds
+    -Wunused-packages
+    -Wunused-type-patterns
+    -Wno-unticked-promoted-constructors
 
 optimization: 2
 test-show-details: direct
@@ -13,55 +24,56 @@ test-show-details: direct
 source-repository-package
     type: git
     location: https://github.com/tbidne/algebra-simple.git
-    tag: 0e7b02ba5b420d7421c8f652aec5d219d0402395
+    tag: a23ee42d30893dee44e96308c49e9441edfa052b
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/bounds.git
-    tag: 1d7451a64d38d3998ce647075663fb81fdf34ad4
+    tag: 3223a637ab618a2bbf3c40ca0225484043a7bacb
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/monad-effects.git
-    tag: 85a5694c59cc130b185101f98aec1ce4c7e80733
+    tag: 7af8e1f9e75f967083839dbdcbbbe4d75ddecd60
     subdir:
-        effects-env
-        effects-exceptions
-        effects-async
-        effects-fs
-        effects-ioref
-        effects-logger-ns
-        effects-optparse
-        effects-stm
-        effects-time
-        effects-terminal
-        effects-thread
-        effects-typed-process
+        lib/effects-env
+        lib/effects-exceptions
+        lib/effects-async
+        lib/effects-fs
+        lib/effects-ioref
+        lib/effects-logger-ns
+        lib/effects-optparse
+        lib/effects-stm
+        lib/effects-time
+        lib/effects-terminal
+        lib/effects-thread
+        lib/effects-typed-process
+        lib/effects-unix-compat
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/pythia.git
-    tag: 2a4de1465c75513617293ca23b3541e4be4b82cb
+    tag: 0e6e30c63f1fe403ec841780a8e4371fedddf63f
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/relative-time.git
-    tag: e9dbac1861ce4db3e40c8bf5109b1fabfab24c22
+    tag: 5f89851baf90c671fffa8d5118e0b37186220fef
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/si-bytes.git
-    tag: 8f0de55f84f5324477072939181df5fc520a0e0a
+    tag: b78349ef776b1ee80ed31d9e27ec3e5bf91245b1
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/smart-math.git
-    tag: 068d8a72b86df3f64fa0bc2df85d1add9f0d47a8
+    tag: fdeac16ca3a05ac1a1211fab9fe8de5a91aeb7f7
 
 source-repository-package
     type: git
     location: https://github.com/tbidne/time-conv.git
-    tag: 19628566adcf9bfd1ed87a407aa0d5798ca94df0
+    tag: 796a4f58a1e6017f99d09b6c2a95b0e5d7dae5e9
 
 allow-newer: dbus:template-haskell
 allow-newer: dbus:transformers

flake.nix
@@ -91,13 +91,13 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       perSystem = { pkgs, ... }:
         let
-          ghc-version = "ghc962";
+          ghc-version = "ghc963";
           compiler = pkgs.haskell.packages."${ghc-version}".override {
             overrides = final: prev: {
-              file-io = final.callHackage "file-io" "0.1.0.1" { };
-              hedgehog = prev.hedgehog_1_3;
+              hedgehog = prev.hedgehog_1_4;
               hlint = prev.hlint_3_6_1;
-              ormolu = prev.ormolu_0_7_1_0;
+              ormolu = prev.ormolu_0_7_2_0;
+              tasty-hedgehog = prev.tasty-hedgehog_1_4_0_2;
             } // nix-hs-utils.mkLibs inputs final [
               "algebra-simple"
               "bounds"
@@ -106,7 +106,7 @@
               "si-bytes"
               "smart-math"
               "time-conv"
-            ] // nix-hs-utils.mkRelLibs monad-effects final [
+            ] // nix-hs-utils.mkRelLibs "${monad-effects}/lib" final [
               "effects-async"
               "effects-env"
               "effects-exceptions"
@@ -119,6 +119,7 @@
               "effects-terminal"
               "effects-thread"
               "effects-typed-process"
+              "effects-unix-compat"
             ];
           };
           hlib = pkgs.haskell.lib;
@@ -128,22 +129,16 @@
               name = "navi";
               root = ./.;
             };
-          hsDirs = "app src test";
+          compilerPkgs = { inherit compiler pkgs; };
         in
         {
           packages.default = mkPkg false;
           devShells.default = mkPkg true;
 
           apps = {
-            format = nix-hs-utils.format {
-              inherit compiler hsDirs pkgs;
-            };
-            lint = nix-hs-utils.lint {
-              inherit compiler hsDirs pkgs;
-            };
-            lint-refactor = nix-hs-utils.lint-refactor {
-              inherit compiler hsDirs pkgs;
-            };
+            format = nix-hs-utils.format compilerPkgs;
+            lint = nix-hs-utils.lint compilerPkgs;
+            lintRefactor = nix-hs-utils.lintRefactor compilerPkgs;
           };
         };
       systems = [

navi.cabal
@@ -24,15 +24,15 @@ source-repository head
 
 common common-exts
   default-extensions:
-    NoFieldSelectors
-    NoImplicitPrelude
-    NoStarIsType
     ApplicativeDo
     DataKinds
     DerivingVia
     DuplicateRecordFields
     FunctionalDependencies
     LambdaCase
+    NoFieldSelectors
+    NoImplicitPrelude
+    NoStarIsType
     OverloadedLabels
     OverloadedStrings
     TypeFamilies
@@ -101,7 +101,6 @@ library
     , pythia                ^>=0.1
     , relative-time         ^>=0.1
     , si-bytes              ^>=0.1
-    , smart-math            ^>=0.1
     , text                  >=1.2.3.2   && <2.1
     , toml-reader           >=0.1.0.0   && <0.3
 
@@ -143,7 +142,6 @@ test-suite unit
     , fdo-notify
     , navi
     , pythia
-    , smart-math
     , tasty        >=1.1.0.3 && <1.5
     , tasty-hunit  >=0.9     && <0.11
     , text
@@ -172,7 +170,6 @@ test-suite integration
     , fdo-notify
     , navi
     , pythia
-    , smart-math
     , tasty
     , tasty-hunit
     , text

src/Navi/Effects/MonadSystemInfo.hs
@@ -50,7 +50,7 @@ queryMultiple cmd =
   let shellApp = multipleShellApp cmd
    in T.strip <$> ShellApp.runSimple shellApp
 
-multipleShellApp :: Command -> SimpleShell EventError Text
+multipleShellApp :: (Applicative f) => Command -> SimpleShell f EventError Text
 multipleShellApp cmd =
   MkSimpleShell
     { command = cmd,
@@ -66,7 +66,7 @@ querySingle cmd = do
   let shellApp = singleShellApp cmd
    in T.strip <$> ShellApp.runSimple shellApp
 
-singleShellApp :: Command -> SimpleShell EventError Text
+singleShellApp :: (Applicative f) => Command -> SimpleShell f EventError Text
 singleShellApp cmd =
   MkSimpleShell
     { command = cmd,

src/Navi/Services/Battery/Percentage.hs
@@ -13,9 +13,12 @@ import Navi.Data.PollInterval (PollInterval (..))
 import Navi.Event.Toml qualified as EventToml
 import Navi.Event.Types (AnyEvent (..), ErrorNote, Event (..), RepeatEvent)
 import Navi.Prelude
-import Navi.Services.Battery.Percentage.Toml (BatteryPercentageNoteToml (..), BatteryPercentageToml)
+import Navi.Services.Battery.Percentage.Toml
+  ( BatteryPercentageNoteToml (..),
+    BatteryPercentageToml,
+  )
 import Navi.Services.Types (ServiceType (..))
-import Numeric.Data.Interval qualified as Interval
+import Pythia.Data.Percentage qualified as Percentage
 import Pythia.Services.Battery
   ( Battery (..),
     BatteryApp,
@@ -50,7 +53,7 @@ tomlToNote toml =
     summary = "Battery Percentage"
     body =
       Just
-        $ showt (Interval.unLRInterval $ percentage ^. #unPercentage)
+        $ showt (Percentage.unPercentage percentage)
         <> "%"
 
 mkBatteryEvent ::

src/Navi/Services/Battery/Percentage/Toml.hs
@@ -21,7 +21,7 @@ import Navi.Event.Toml
 import Navi.Prelude
 import Navi.Services.Battery.Common (batteryAppDecoder)
 import Navi.Utils (urgencyLevelOptDecoder)
-import Numeric.Data.Interval qualified as Interval
+import Pythia.Data.Percentage qualified as Percentage
 import Pythia.Services.Battery (BatteryApp (..), Percentage (..))
 
 -- | TOML for each individual battery percentage.
@@ -50,7 +50,7 @@ percentageDecoder = getFieldWith decoder "percent"
   where
     decoder =
       tomlDecoder >>= \x ->
-        case mkPercentage x of
+        case Percentage.mkPercentage x of
           Just n -> pure n
           Nothing ->
             fail
@@ -60,7 +60,6 @@ percentageDecoder = getFieldWith decoder "percent"
                   showt x,
                   ". Expected integer in [0, 100]."
                 ]
-    mkPercentage = fmap MkPercentage . Interval.mkLRInterval
 
 -- | TOML for the battery percentage service.
 data BatteryPercentageToml = MkBatteryPercentageToml

commit e412c0a820fdffedc28bb18fce1d741f8b571d41

Fix xdg state log path
991ad9a30be3fb5c8d86c13a0c1e3b01fc8b813b broke default logging to
XDG state dir. This fixes it.

app/Main.hs
@@ -115,7 +115,7 @@ withLogHandle logging onMHandle = do
       withBinaryFile f WriteMode $ \h -> onMHandle (Just h)
     -- Use the default log path: xdgState </> navi/log
     DefPath -> do
-      xdgState <- Dir.getXdgState [osp|navi/|]
+      xdgState <- Dir.getXdgState [osp|navi|]
 
       -- handle large log dir
       handleLogSize xdgState sizeMode
@@ -124,9 +124,10 @@ withLogHandle logging onMHandle = do
         encodeFpToValidOsThrowM
           . fmap replaceSpc
           =<< Time.getSystemTimeString
-      let logFile = currTimeOs <> [osp|.log|]
+      let logFile = xdgState </> currTimeOs <> [osp|.log|]
       stateExists <- Dir.doesDirectoryExist xdgState
       unless stateExists (Dir.createDirectoryIfMissing True xdgState)
+
       renameIfExists logFile
       withBinaryFile logFile WriteMode $ \h -> onMHandle (Just h)
   where