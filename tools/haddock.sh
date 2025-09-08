5a488fc5d5d5b786a81674250a61902bba552180
Tommy Bidne <tbidne@protonmail.com>
2025-09-08 15:22:15 +1200

    Fix doc script

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -2,7 +2,6 @@ set -e
 
 export LANG="C.UTF-8"
 
-cabal update
 cabal haddock --haddock-hyperlink-source --haddock-quickjump
 
 mkdir -p docs/
@@ -10,4 +9,4 @@ mkdir -p docs/
 # shellcheck disable=SC2038
 find docs/ -type f | xargs -I % sh -c "rm -r %"
 
-cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.4/navi-0.1/doc/html/navi/* docs/
+cp -r dist-newstyle/build/x86_64-linux/ghc-*/navi-*/opt/doc/html/navi/* docs/
